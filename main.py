642247c70b4b050492677618455b2ebf2f8bf4bc
Jonas Steeck <jonas.steeck@gmx.de>
2023-06-20 15:07:50 +0200

    Transfered update from master branch

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, jsonify
 from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+import logging
 
+logging.basicConfig(
+    level=logging.INFO
+)
 app = Flask(__name__)
+lemmatizer = WordNetLemmatizer()
 
 @app.route('/synonyms', methods=['GET'])
 def get_synonyms():
@@ -15,5 +21,15 @@ def get_synonyms():
     synonyms = list(set(synonyms))
     return jsonify(synonyms)
 
+@app.route('/baseform', methods=['POST'])
+def get_baseform():
+    data = request.get_json()
+    word = data.get('word')
+    pos = data.get('pos')
+    logging.info(f"Looking for baseword of {word} ({pos})")
+    base_form = lemmatizer.lemmatize(word, pos)
+    logging.info(f"Returning: {base_form}")
+    return jsonify({"word": base_form})
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
